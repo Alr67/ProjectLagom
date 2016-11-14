5f48487d9d055474d63f200ff2a2f52d85f9f3a8
Erik Markström <erimar@kth.se>
2016-11-14 12:26:05 +0100

    snowballthrower operational

---
+++ Project Lagom/Assets/Scripts/ThrowScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ThrowScript : MonoBehaviour {
+
+	public Button yourButton;
+	public GameObject snowball;
+	private GameObject arrow;
+	private float arrowDirection;
+	private Vector3 throwDirection;
+
+	void Start () {
+		Button btn = yourButton.GetComponent<Button>();
+		arrow = GameObject.FindGameObjectWithTag("Arrow");
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		Debug.Log ("You have clicked the button!");
+
+		Instantiate(snowball, arrow.transform.position, arrow.transform.rotation);
+
+
+	}
+}
