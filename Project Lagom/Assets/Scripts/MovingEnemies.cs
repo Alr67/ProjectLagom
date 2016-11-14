2cdca7686a61e986a71c363403a1f32b534924bc
Rafa <rafael.lucena@est.fib.upc.edu>
2016-11-14 12:07:15 +0100

    Vandal collision

---
+++ Project Lagom/Assets/Scripts/MovingEnemies.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingEnemies : MonoBehaviour {
+    public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Goat"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
