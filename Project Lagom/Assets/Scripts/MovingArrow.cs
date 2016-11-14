25de925392349f8335b2b5ebe78d92f25fe6fe5f
Rafa <rafael.lucena@est.fib.upc.edu>
2016-11-14 11:18:34 +0100

    Working rotation

--- Project Lagom/Assets/Scripts/MovingArrow.cs
+++ Project Lagom/Assets/Scripts/MovingArrow.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class MovingArrow : MonoBehaviour {
     public float speed = 15f;
+    public bool right = true;
     // Use this for initialization
     void Start () {
 	    
@@ -10,6 +11,13 @@ public class MovingArrow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+        if (right && transform.eulerAngles.y > 120f) {
+            right = false;
+        }
+        else if (!right && transform.eulerAngles.y < 60f) {
+            right = true;
+        }
+        if(!right) transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+        else transform.Rotate(-Vector3.forward, speed * Time.deltaTime);
     }
 }
