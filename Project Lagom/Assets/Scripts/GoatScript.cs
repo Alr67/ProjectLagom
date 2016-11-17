146995364e5ea811c5d455bb6195c1b509ee86cd
Rafa <rafael.lucena@est.fib.upc.edu>
2016-11-17 10:29:26 +0100

    Minor fixes

--- Project Lagom/Assets/Scripts/GoatScript.cs
+++ Project Lagom/Assets/Scripts/GoatScript.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;using UnityEngine.UI;using System.Collections;public class GoatScript : MonoBehaviour {    public int lifePoints;    public Text lifeText;    public Text winLostText;    public int killedEnemies;    public Text killedEnemiesText;	// Use this for initialization	void Start () {        lifePoints = 4;        winLostText.enabled = false;        killedEnemies = 0;	}		// Update is called once per frame	void Update () {		}    void OnTriggerEnter(Collider other) {        if (other.gameObject.CompareTag("Vandal")) {			Destroy (other.gameObject);            --lifePoints;            lifeText.text = "HP: " + lifePoints.ToString();            if (lifePoints < 2) lifeText.color = Color.red;            if (lifePoints == 0) {				Destroy(gameObject);                winLostText.enabled = true;                winLostText.text = "Jul was ruined :C";            }                    }    }    public bool lifePointsChecker()
+﻿using UnityEngine;using UnityEngine.UI;using System.Collections;public class GoatScript : MonoBehaviour {    public int lifePoints;    public Text lifeText;    public Text winLostText;    public int killedEnemies;    public Text killedEnemiesText;    public Button restartButton;	// Use this for initialization	void Start () {        lifePoints = 4;        winLostText.enabled = false;        killedEnemies = 0;        restartButton.enabled = false;        restartButton.gameObject.SetActive(false);	}		// Update is called once per frame	void Update () {		}    void OnTriggerEnter(Collider other) {        if (other.gameObject.CompareTag("Vandal")) {			Destroy (other.gameObject);            --lifePoints;            lifeText.text = "HP: " + lifePoints.ToString();            if (lifePoints < 2) lifeText.color = Color.red;            if (lifePoints == 0) {				Destroy(gameObject);                winLostText.enabled = true;                winLostText.text = "Jul was ruined :C";                restartButton.enabled = true;                restartButton.gameObject.SetActive(true);            }                    }    }    public bool lifePointsChecker()
     {
         if (lifePoints > 0) return true;
         else return false;
@@ -10,6 +10,8 @@
         {
             winLostText.enabled = true;
             winLostText.text = "You saved the Gävlebock!";
+            restartButton.enabled = true;
+            restartButton.gameObject.SetActive(true);
         }
     }    public bool keepSpawning()
     {
--- Project Lagom/Assets/Scripts/MovingEnemies.cs
+++ Project Lagom/Assets/Scripts/MovingEnemies.cs
@@ -6,7 +6,7 @@ public class MovingEnemies : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        speed = Random.Range(1f,6f);
+        speed = Random.Range(1.8f,6.5f);
 	}
 	
 	// Update is called once per frame
